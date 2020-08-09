9b6c9bb919c3c7a38aa4fe529b0e21889456bfcb
yoshimitsu.masugata <msgtysmt@gmail.com>
2020-08-09 20:53:06 +0900

    vuejsに移植

---
+++ application/app/javascript/packs/vues/PieChart.vue
@@ -0,0 +1,17 @@
+<script>
+import { Pie, mixins } from 'vue-chartjs'
+
+export default {
+  extends: Pie,
+  mixins: [mixins.reactiveData],
+  props: ['data', 'options'],
+  mounted () {
+    this.renderChart(this.data, this.options);
+  },
+  watch: {
+    data: function() {
+      this.renderChart(this.data, this.options);
+    }
+  }
+}
+</script>
