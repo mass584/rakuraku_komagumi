11fb8f3829d60e5b6e3df8b1611989dcfccd3d37
yoshimitsu.masugata <msgtysmt@gmail.com>
2021-03-31 17:42:33 +0900

    バグ修正

--- application/spec/models/term_tutorial_spec.rb
+++ application/spec/models/term_tutorial_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe TermTutorial, type: :model do
   describe '個別授業の追加' do
     before :each do
       @term = create_normal_term_with_teacher_and_student(0, 2)
-      @tutorial = Factory.create(:tutorial, room: @term.room)
+      @tutorial = FactoryBot.create(:tutorial, room: @term.room)
     end
 
     it '個別授業を新規に追加した場合、TutorialContractが生成される' do
