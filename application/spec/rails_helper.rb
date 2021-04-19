636bcf4d34b19d021194148d1298b8a0ecb2311f
yoshimitsu.masugata <msgtysmt@gmail.com>
2021-04-19 16:01:37 +0900

    chromeをheadless_modeに設定

--- application/spec/rails_helper.rb
+++ application/spec/rails_helper.rb
@@ -61,4 +61,10 @@ RSpec.configure do |config|
 
   config.include FactorySupport
   config.include StubSupport
+
+  config.before(:each) do |example|
+    if example.metadata[:type] == :system
+      driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+    end
+  end
 end
