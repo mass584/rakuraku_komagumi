b7c9beb1594adbccf9b6f48f2ff0c294ea7f4b9f
yoshimitsu.masugata <msgtysmt@gmail.com>
2021-04-23 23:30:44 +0900

    room_controllerのリファクタリング

--- application/app/controllers/rooms_controller.rb
+++ application/app/controllers/rooms_controller.rb
@@ -1,15 +1,11 @@
 class RoomsController < ApplicationController
-  PAGE_SIZE = 10
 
   before_action :authenticate_user!
   before_action :set_rooms!
   before_action :set_room!
 
   def index
-    @keyword = sanitize_string_query_param(params[:keyword])
-    @page = sanitize_integer_query_param(params[:page])
-    @page_size = PAGE_SIZE
-    @rooms = Room.ordered.matched(@keyword).pagenated(@page, @page_size)
+    @rooms = Room.ordered
   end
 
   def create
--- application/app/models/room.rb
+++ application/app/models/room.rb
@@ -9,14 +9,4 @@ class Room < ApplicationRecord
             length: { minimum: 1, maximum: 20 }
 
   scope :ordered, -> { order(created_at: 'ASC') }
-  scope :matched, lambda { |keyword|
-    keyword.instance_of?(String) && keyword.present? ?
-      where('name like ?', "%#{sanitize_sql_like(keyword)}%") :
-      itself
-  }
-  scope :pagenated, lambda { |page, page_size|
-    page.instance_of?(Integer) && page_size.instance_of?(Integer) ?
-      offset((page - 1) * page_size).limit(page_size) :
-      itself
-  }
 end
