93291029f930de6ce588ad2baa1b582b61b796de
yoshimitsu.masugata <msgtysmt@gmail.com>
2021-04-26 22:12:32 +0900

    TutorialPieceModelの追加

---
+++ optimization/src/model/tutorial_piece.py
@@ -0,0 +1,24 @@
+import psycopg2
+import psycopg2.extras
+import logging
+
+logger = logging.getLogger('Term')
+
+class TutorialPiece():
+    def __init__(self, database, tutorial_piece_id):
+        self.database = database
+        self.tutorial_piece_id = tutorial_piece_id
+
+    def update_all()
+        self.database.connect()
+        self.update_seat_id()
+        self.database.commit()
+        self.database.close()
+
+    def update_seat_id(self, seat_id):
+        self.database.connect()
+        cur = self.database.cursor()
+        cur.execute(f"update tutorial_pieces set seat_id = {seat_id} where id = {self.tutorial_piece_id}")
+        cur.close()
+        self.database.commit()
+        self.database.close()
