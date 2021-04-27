8d065279dca77801702c241014870d79ec8ad302
yoshimitsu.masugata <msgtysmt@gmail.com>
2021-04-27 18:33:22 +0900

    array_sizeの導入

---
+++ optimization/src/array_builder/array_size.py
@@ -0,0 +1,16 @@
+import logging
+
+logger = logging.getLogger('Term')
+
+class ArraySize():
+    def __init__(self, term):
+        self.term = term
+        self.__build_counts()
+
+    def __build_counts(self):
+        self.teacher_count = len(self.term.term_teachers)
+        self.student_count = len(self.term.term_students)
+        self.tutorial_count = len(self.term.term_tutorials)
+        self.group_count = len(self.term.term_groups)
+        self.date_count = (self.term.term['end_at'] - self.term.term['begin_at']).days + 1
+        self.period_count = self.term.term['period_count']
--- optimization/src/array_builder/group_occupation.py
+++ optimization/src/array_builder/group_occupation.py
@@ -1,47 +1,46 @@
-import logging
+import numpy as np
 import itertools
 from array_contract import term_student_index, term_teacher_index
 
-logger = logging.getLogger('ArrayIndex')
-
 class GroupOccupation():
-    def __init__(self, term):
+    def __init__(self, term, array_size):
         self.term = term
+        self.array_size = array_size
         self.__build_student_occupation
         self.__build_teacher_occupation
 
     def __build_student_occupation(self):
         self.student_occupation = np.zeros(
-            (self.term.term_student_count, self.term.date_count, self.term.period_count),
+            (self.array_size.student_count, self.array_size.date_count, self.array_size.period_count),
             dtype=int)
-        term_student_list = list(range(self.term.term_student_count))
-        date_index_list = list(range(self.term.date_count))
-        period_index_list = list(range(self.term.period_count))
-        term_group_index_list = list(range(self.term.term_group_count))
-        product = itertools.product(term_student_list, date_index_list, period_index_list, term_group_index_list)
-        for term_student_index, date_index, period_index, term_group_index in product:
+        student_index_list = list(range(self.array_size.student_count))
+        date_index_list = list(range(self.array_size.date_count))
+        period_index_list = list(range(self.array_size.period_count))
+        group_index_list = list(range(self.array_size.group_count))
+        product = itertools.product(student_index_list, date_index_list, period_index_list, group_index_list)
+        for student_index, date_index, period_index, group_index in product:
             is_matched = (lambda timetable:
                 timetable['date_index'] == date_index and
                 timetable['period_index'] == period_index and
-                timetable['term_group_index'] == term_group_index and
-                timetable['term_student_index'] == term_student_index)
+                timetable['term_group_index'] == group_index and
+                timetable['term_student_index'] == student_index)
             is_contracted = True in [True for timetable in self.term.timetables if is_matched(timetable)]
-            if is_contracted: self.student_occupation[term_student_index, date_index, period_index] = 1
+            if is_contracted: self.student_occupation[student_index, date_index, period_index] = 1
 
     def __build_teacher_occupation(self):
         self.teacher_occupation = np.zeros(
-            (self.term.term_teacher_count, self.term.date_count, self.term.period_count)
+            (self.array_size.teacher_count, self.array_size.date_count, self.array_size.period_count)
             dtype=int)
-        term_teacher_list = list(range(self.term.term_teacher_count))
-        date_index_list = list(range(self.term.date_count))
-        period_index_list = list(range(self.term.period_count))
-        term_group_index_list = list(range(self.term.term_group_count))
-        product = itertools.product(term_teacher_list, date_index_list, period_index_list, term_group_index_list)
+        teacher_index_list = list(range(self.array_size.teacher_count))
+        date_index_list = list(range(self.array_size.date_count))
+        period_index_list = list(range(self.array_size.period_count))
+        group_index_list = list(range(self.array_size.group_count))
+        product = itertools.product(teacher_index_list, date_index_list, period_index_list, group_index_list)
         for term_teacher_index, date_index, period_index, term_group_index in product:
             is_matched = (lambda timetable:
                 timetable['date_index'] == date_index and
                 timetable['period_index'] == period_index and
-                timetable['term_group_index'] == term_group_index and
-                timetable['term_teacher_index'] == term_teacher_index)
+                timetable['term_group_index'] == group_index and
+                timetable['term_teacher_index'] == teacher_index)
             is_contracted = True in [True for timetable in self.term.timetables if is_matched(timetable)]
-            if is_contracted: self.teacher_occupation[term_teacher_index, date_index, period_index] = 1
+            if is_contracted: self.teacher_occupation[teacher_index, date_index, period_index] = 1
