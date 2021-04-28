a1f6991ca2c5a2ea3f3182e51e247b0f71561206
yoshimitsu.masugata <msgtysmt@gmail.com>
2021-04-29 01:47:14 +0900

    テスト追加

--- optimization/src/array_builder/tutorial_contract.py
+++ optimization/src/array_builder/tutorial_contract.py
@@ -3,22 +3,28 @@ import numpy
 
 class TutorialContract():
     def __init__(self, term, array_size):
-        self.term = term
-        self.array_size = array_size
+        self.__term = term
+        self.__array_size = array_size
         self.__build_tutorial_contract()
 
     def __build_tutorial_contract(self):
-        self.tutorial_contract_array = numpy.zeros(
-            (self.array_size.student_count, self.array_size.tutorial_count),
+        self.__tutorial_contract_array = numpy.zeros(
+            (self.__array_size.student_count(), self.__array_size.tutorial_count()),
             dtype=int)
-        student_index_list = list(range(self.array_size.student_count))
-        tutorial_index_list = list(range(self.array_size.tutorial_count))
+        student_index_list = list(range(self.__array_size.student_count()))
+        tutorial_index_list = list(range(self.__array_size.tutorial_count()))
         product = itertools.product(student_index_list, tutorial_index_list)
         for student_index, tutorial_index in product:
-            term_student_id = self.term.term_students[student_index]['id']
-            term_tutorial_id = self.term.term_tutorials[tutorial_index]['id']
+            term_student_id = self.__term['term_students'][student_index]['id']
+            term_tutorial_id = self.__term['term_tutorials'][tutorial_index]['id']
             is_matched = (lambda tutorial_contract:
                 tutorial_contract['term_student_id'] == term_student_id and
                 tutorial_contract['term_tutorial_id'] == term_tutorial_id)
-            piece_count = next(tutorial_contract['piece_count'] for tutorial_contract in self.term.tutorial_contracts if is_matched(tutorial_contract))
-            self.tutorial_contract_array[student_index, tutorial_index] = piece_count
+            piece_count = next(
+                tutorial_contract['piece_count']
+                for tutorial_contract in self.__term['tutorial_contracts']
+                if is_matched(tutorial_contract))
+            self.__tutorial_contract_array[student_index, tutorial_index] = piece_count
+
+    def tutorial_contract_array(self):
+        return self.__tutorial_contract_array
---
+++ optimization/test/array_builder/test_tutorial_contract.py
@@ -0,0 +1,23 @@
+import numpy
+from unittest import TestCase
+from test.test_data.normal_term import normal_term
+from test.test_data.season_term import season_term
+from test.test_data.exam_planning_term import exam_planning_term
+from src.array_builder.array_size import ArraySize
+from src.array_builder.tutorial_contract import TutorialContract
+
+class TestTutorialContract(TestCase):
+    def test_normal_term(self):
+        array_size = ArraySize(normal_term)
+        tutorial_contract = TutorialContract(normal_term, array_size)
+        self.assertEqual(numpy.sum(tutorial_contract.tutorial_contract_array()), 60)
+
+    def test_season_term(self):
+        array_size = ArraySize(season_term)
+        tutorial_contract = TutorialContract(season_term, array_size)
+        self.assertEqual(numpy.sum(tutorial_contract.tutorial_contract_array()), 240)
+
+    def test_exam_planning_term(self):
+        array_size = ArraySize(exam_planning_term)
+        tutorial_contract = TutorialContract(exam_planning_term, array_size)
+        self.assertEqual(numpy.sum(tutorial_contract.tutorial_contract_array()), 20)
