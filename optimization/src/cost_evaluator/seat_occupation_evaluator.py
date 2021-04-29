53b8fc302391fa6b2ea97681c0b48c766b34959f
yoshimitsu.masugata <msgtysmt@gmail.com>
2021-04-29 14:54:08 +0900

    seat_occupation_evaluatorのテスト追加

--- optimization/src/cost_evaluator/seat_occupation_evaluator.py
+++ optimization/src/cost_evaluator/seat_occupation_evaluator.py
@@ -1,18 +1,19 @@
 import itertools
-import numpy as np
+import numpy
 
 class SeatOccupationEvaluator():
     def __init__(self, array_size):
-        self.array_size = array_size
+        self.__array_size = array_size
 
     def violation_and_cost(self, tutorial_occupation):
-        occupation1 = np.einsum('ijkml->jml', tutorial_occupation)
-        theta = lambda x: np.heaviside(x, 0).astype(int)
-        occupation2 = np.sum(np.apply_along_axis(theta, 1, occupation1), axis=0)
-        date_index_list = range(self.array_size.date_count)
-        period_index_list = range(self.array_size.period_count)
+        occupation1 = numpy.einsum('ijkml->jml', tutorial_occupation)
+        theta = lambda x: numpy.heaviside(x, 0).astype(int)
+        occupation2 = numpy.sum(numpy.apply_along_axis(theta, 1, occupation1), axis=0)
+        date_index_list = range(self.__array_size.date_count())
+        period_index_list = range(self.__array_size.period_count())
         product = itertools.product(date_index_list, period_index_list)
+        violation_summation = 0
         for date_index, period_index in product:
-            excess = occupation2[date_index, period_index] - self.array_size.seat_count
+            excess = occupation2[date_index, period_index] - self.__array_size.seat_count()
             if excess > 0: violation_summation += excess
         return [violation_summation, 0]
---
+++ optimization/test/cost_evaluator/test_seat_occupation_evaluator.py
@@ -0,0 +1,69 @@
+import copy
+from unittest import TestCase
+from test.test_data.normal_term import normal_term
+from test.test_data.season_term import season_term
+from test.test_data.exam_planning_term import exam_planning_term
+from src.array_builder.array_size import ArraySize
+from src.array_builder.tutorial_occupation import TutorialOccupation
+from src.cost_evaluator.seat_occupation_evaluator import SeatOccupationEvaluator
+
+class TestSeatOccupationEvaluator(TestCase):
+    def test_fully_occupied_seat(self):
+        term = copy.deepcopy(season_term)
+        term['term']['seat_count'] = 2
+        term['tutorial_pieces'][0]['date_index'] = 1
+        term['tutorial_pieces'][0]['period_index'] = 1
+        term['tutorial_pieces'][0]['term_teacher_id'] = 1
+        term['tutorial_pieces'][4]['date_index'] = 1
+        term['tutorial_pieces'][4]['period_index'] = 1
+        term['tutorial_pieces'][4]['term_teacher_id'] = 2
+        term['tutorial_pieces'][8]['date_index'] = 1
+        term['tutorial_pieces'][8]['period_index'] = 1
+        term['tutorial_pieces'][8]['term_teacher_id'] = 2
+        array_size = ArraySize(term)
+        tutorial_occupation = TutorialOccupation(term, array_size)
+        evaluator = SeatOccupationEvaluator(array_size) 
+        self.assertEqual(evaluator.violation_and_cost(
+            tutorial_occupation.tutorial_occupation()
+        ), [0, 0])
+
+    def test_lack_of_one_seat(self):
+        term = copy.deepcopy(season_term)
+        term['term']['seat_count'] = 2
+        term['tutorial_pieces'][0]['date_index'] = 1
+        term['tutorial_pieces'][0]['period_index'] = 1
+        term['tutorial_pieces'][0]['term_teacher_id'] = 1
+        term['tutorial_pieces'][4]['date_index'] = 1
+        term['tutorial_pieces'][4]['period_index'] = 1
+        term['tutorial_pieces'][4]['term_teacher_id'] = 2
+        term['tutorial_pieces'][8]['date_index'] = 1
+        term['tutorial_pieces'][8]['period_index'] = 1
+        term['tutorial_pieces'][8]['term_teacher_id'] = 3
+        array_size = ArraySize(term)
+        tutorial_occupation = TutorialOccupation(term, array_size)
+        evaluator = SeatOccupationEvaluator(array_size) 
+        self.assertEqual(evaluator.violation_and_cost(
+            tutorial_occupation.tutorial_occupation()
+        ), [1, 0])
+
+    def test_lack_of_two_seat(self):
+        term = copy.deepcopy(season_term)
+        term['term']['seat_count'] = 2
+        term['tutorial_pieces'][0]['date_index'] = 1
+        term['tutorial_pieces'][0]['period_index'] = 1
+        term['tutorial_pieces'][0]['term_teacher_id'] = 1
+        term['tutorial_pieces'][4]['date_index'] = 1
+        term['tutorial_pieces'][4]['period_index'] = 1
+        term['tutorial_pieces'][4]['term_teacher_id'] = 2
+        term['tutorial_pieces'][8]['date_index'] = 1
+        term['tutorial_pieces'][8]['period_index'] = 1
+        term['tutorial_pieces'][8]['term_teacher_id'] = 3
+        term['tutorial_pieces'][12]['date_index'] = 1
+        term['tutorial_pieces'][12]['period_index'] = 1
+        term['tutorial_pieces'][12]['term_teacher_id'] = 4
+        array_size = ArraySize(term)
+        tutorial_occupation = TutorialOccupation(term, array_size)
+        evaluator = SeatOccupationEvaluator(array_size) 
+        self.assertEqual(evaluator.violation_and_cost(
+            tutorial_occupation.tutorial_occupation()
+        ), [2, 0])
