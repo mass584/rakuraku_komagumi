8d84a0d65115c1fb306f55935c50f4ecbf41cc17
yoshimitsu.masugata <msgtysmt@gmail.com>
2021-04-29 16:07:36 +0900

    vacancy_and_double_booking_evaluatorのテスト追加

--- optimization/src/cost_evaluator/vacancy_and_double_booking_evaluator.py
+++ optimization/src/cost_evaluator/vacancy_and_double_booking_evaluator.py
@@ -1,16 +1,21 @@
-import numpy as np
+import numpy
 
 class VacancyAndDoubleBookingEvaluator():
-    def student_evaluation(self, tutorial_occupation, student_vacancy):
-        student_occupation = np.einsum('ijkml->iml', tutorial_occupation)
-        relu = lambda x: np.maximum(0, x).astype(int)
-        excess = np.apply_along_axis(relu, 1, student_occupation - student_vacancy)
-        violation = np.sum(excess)
+    def __student_violation_and_cost(self, tutorial_occupation, student_vacancy):
+        student_occupation = numpy.einsum('ijkml->iml', tutorial_occupation)
+        relu = lambda x: numpy.maximum(0, x).astype(int)
+        excess = numpy.apply_along_axis(relu, 1, student_occupation - student_vacancy)
+        violation = numpy.sum(excess)
         return [violation, 0]
 
-    def teacher_evaluation(self, tutorial_occupation, teacher_vacancy):
-        teacher_occupation = np.einsum('ijkml->jml', tutorial_occupation)
-        relu = lambda x: np.maximum(0, x).astype(int)
-        excess = np.apply_along_axis(relu, 1, teacher_occupation - 2 * teacher_vacancy)
-        violation = np.sum(excess)
+    def __teacher_violation_and_cost(self, tutorial_occupation, teacher_vacancy):
+        teacher_occupation = numpy.einsum('ijkml->jml', tutorial_occupation)
+        relu = lambda x: numpy.maximum(0, x).astype(int)
+        excess = numpy.apply_along_axis(relu, 1, teacher_occupation - 2 * teacher_vacancy)
+        violation = numpy.sum(excess)
         return [violation, 0]
+
+    def violation_and_cost(self, tutorial_occupation, teacher_vacancy, student_vacancy):
+        teacher_violation_and_cost = self.__teacher_violation_and_cost(tutorial_occupation, teacher_vacancy)
+        student_violation_and_cost = self.__student_violation_and_cost(tutorial_occupation, student_vacancy)
+        return [teacher + student for teacher, student in zip(teacher_violation_and_cost, student_violation_and_cost)] 
---
+++ optimization/test/cost_evaluator/test_vacancy_and_double_booking_evaluator.py
@@ -0,0 +1,61 @@
+import copy
+from unittest import TestCase
+from test.test_data.season_term import season_term
+from src.array_builder.array_size import ArraySize
+from src.array_builder.tutorial_occupation import TutorialOccupation
+from src.array_builder.vacancy import Vacancy
+from src.cost_evaluator.vacancy_and_double_booking_evaluator import VacancyAndDoubleBookingEvaluator
+
+class TestVacancyAndDoubleBookingEvaluator(TestCase):
+    def test_no_violation(self):
+        term = copy.deepcopy(season_term)
+        term['tutorial_pieces'][0]['date_index'] = 1
+        term['tutorial_pieces'][0]['period_index'] = 1
+        term['tutorial_pieces'][1]['date_index'] = 1
+        term['tutorial_pieces'][1]['period_index'] = 2
+        array_size = ArraySize(term)
+        vacancy = Vacancy(term, array_size)
+        tutorial_occupation = TutorialOccupation(term, array_size)
+        evaluator = VacancyAndDoubleBookingEvaluator()
+        self.assertEqual(evaluator.violation_and_cost(
+            tutorial_occupation.tutorial_occupation(),
+            vacancy.teacher_vacancy_array(),
+            vacancy.student_vacancy_array(),
+        ), [0, 0])
+
+    def test_double_booking_violation(self):
+        term = copy.deepcopy(season_term)
+        term['tutorial_pieces'][0]['date_index'] = 1
+        term['tutorial_pieces'][0]['period_index'] = 1
+        term['tutorial_pieces'][0]['term_teacher_id'] = 1
+        term['tutorial_pieces'][4]['date_index'] = 1
+        term['tutorial_pieces'][4]['period_index'] = 1
+        term['tutorial_pieces'][4]['term_teacher_id'] = 1
+        term['tutorial_pieces'][8]['date_index'] = 1
+        term['tutorial_pieces'][8]['period_index'] = 1
+        term['tutorial_pieces'][8]['term_teacher_id'] = 1
+        array_size = ArraySize(term)
+        vacancy = Vacancy(term, array_size)
+        tutorial_occupation = TutorialOccupation(term, array_size)
+        evaluator = VacancyAndDoubleBookingEvaluator()
+        self.assertEqual(evaluator.violation_and_cost(
+            tutorial_occupation.tutorial_occupation(),
+            vacancy.teacher_vacancy_array(),
+            vacancy.student_vacancy_array(),
+        ), [3, 0])
+
+    def test_unvacant_violation(self):
+        term = copy.deepcopy(season_term)
+        term['student_vacancies'][0]['is_vacant'] = False
+        term['teacher_vacancies'][0]['is_vacant'] = False
+        term['tutorial_pieces'][0]['date_index'] = 1
+        term['tutorial_pieces'][0]['period_index'] = 1
+        array_size = ArraySize(term)
+        vacancy = Vacancy(term, array_size)
+        tutorial_occupation = TutorialOccupation(term, array_size)
+        evaluator = VacancyAndDoubleBookingEvaluator()
+        self.assertEqual(evaluator.violation_and_cost(
+            tutorial_occupation.tutorial_occupation(),
+            vacancy.teacher_vacancy_array(),
+            vacancy.student_vacancy_array(),
+        ), [2, 0])
