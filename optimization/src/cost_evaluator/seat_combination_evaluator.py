419e2f99fc7823ff53ffb2a5b5a571831fa56d5c
yoshimitsu.masugata <msgtysmt@gmail.com>
2021-04-29 14:27:34 +0900

    テスト追加

--- optimization/src/cost_evaluator/seat_combination_evaluator.py
+++ optimization/src/cost_evaluator/seat_combination_evaluator.py
@@ -3,21 +3,22 @@ import numpy as np
 
 class SeatCombinationEvaluator():
     def __init__(self, array_size, single_cost, different_pair_cost):
-        self.array_size = array_size
-        self.single_cost = single_cost
-        self.different_pair_cost = different_pair_cost
+        self.__array_size = array_size
+        self.__single_cost = single_cost
+        self.__different_pair_cost = different_pair_cost
 
     def violation_and_cost(self, tutorial_occupation):
         occupation = np.einsum('ijkml->jkml', tutorial_occupation)
-        teacher_index_list = range(self.array_size.teacher_count)
-        date_index_list = range(self.array_size.date_count)
-        period_index_list = range(self.array_size.period_count)
+        teacher_index_list = range(self.__array_size.teacher_count())
+        date_index_list = range(self.__array_size.date_count())
+        period_index_list = range(self.__array_size.period_count())
         product = itertools.product(teacher_index_list, date_index_list, period_index_list)
+        cost_summation = 0
         for teacher_index, date_index, period_index in product:
             vector = occupation[teacher_index, :, date_index, period_index]
             summation = sum(vector)
             if summation == 1:
-                cost_summation += self.single_cost
-            elif (summation == 2) and (1 in seat_occupation):
-                cost_summation += self.different_pair_cost
+                cost_summation += self.__single_cost
+            elif (summation == 2) and (1 in vector):
+                cost_summation += self.__different_pair_cost
         return [0, cost_summation]
