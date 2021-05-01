5d7f8f2381629820cc2c341d8bcda3a86fcd9fff
yoshimitsu.masugata <msgtysmt@gmail.com>
2021-05-02 00:46:08 +0900

    is_optimizingの追加

---
+++ application/db/migrate/20210501154329_add_is_optimizing_to_term.rb
@@ -0,0 +1,5 @@
+class AddIsOptimizingToTerm < ActiveRecord::Migration[6.1]
+  def change
+    add_column :terms, :is_optimizing, :boolean, null: false, default: false
+  end
+end
--- application/db/schema.rb
+++ application/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_01_145044) do
+ActiveRecord::Schema.define(version: 2021_05_01_154329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -200,6 +200,7 @@ ActiveRecord::Schema.define(version: 2021_05_01_145044) do
     t.integer "position_count", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_optimizing", default: false, null: false
   end
 
   create_table "timetables", force: :cascade do |t|
