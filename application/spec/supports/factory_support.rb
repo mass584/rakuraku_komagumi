985cd80a4c11dda2cf3d5c2c5d86c1d70db38826
yoshimitsu.masugata <msgtysmt@gmail.com>
2021-05-10 18:23:52 +0900

    factory_supportの修正

--- application/spec/supports/factory_support.rb
+++ application/spec/supports/factory_support.rb
@@ -3,10 +3,10 @@ module FactorySupport
     room = FactoryBot.create(:room)
     term = FactoryBot.create(:first_term, room: room)
     FactoryBot.create_list(:tutorial, 5, room: term.room).map do |tutorial|
-      FactoryBot.create(:term_tutorial, id: tutorial.id, term: term, tutorial: tutorial)
+      FactoryBot.create(:term_tutorial, term: term, tutorial: tutorial)
     end
     FactoryBot.create_list(:group, 2, room: term.room).map do |group|
-      FactoryBot.create(:term_group, id: group.id, term: term, group: group)
+      FactoryBot.create(:term_group, term: term, group: group)
     end
     term.reload
   end
@@ -15,10 +15,10 @@ module FactorySupport
     room = FactoryBot.create(:room)
     term = FactoryBot.create(:spring_term, room: room)
     FactoryBot.create_list(:tutorial, 5, room: term.room).map do |tutorial|
-      FactoryBot.create(:term_tutorial, id: tutorial.id, term: term, tutorial: tutorial)
+      FactoryBot.create(:term_tutorial, term: term, tutorial: tutorial)
     end
     FactoryBot.create_list(:group, 2, room: term.room).map do |group|
-      FactoryBot.create(:term_group, id: group.id, term: term, group: group)
+      FactoryBot.create(:term_group, term: term, group: group)
     end
     term.reload
   end
@@ -27,10 +27,10 @@ module FactorySupport
     room = FactoryBot.create(:room)
     term = FactoryBot.create(:exam_planning_term, room: room)
     FactoryBot.create_list(:tutorial, 5, room: term.room).map do |tutorial|
-      FactoryBot.create(:term_tutorial, id: tutorial.id, term: term, tutorial: tutorial)
+      FactoryBot.create(:term_tutorial, term: term, tutorial: tutorial)
     end
     FactoryBot.create_list(:group, 2, room: term.room).map do |group|
-      FactoryBot.create(:term_group, id: group.id, term: term, group: group)
+      FactoryBot.create(:term_group, term: term, group: group)
     end
     term.reload
   end
@@ -38,10 +38,10 @@ module FactorySupport
   def create_normal_term_with_teacher_and_student(teacher_count, student_count)
     term = create_normal_term
     FactoryBot.create_list(:teacher, teacher_count, room: term.room).map do |teacher|
-      FactoryBot.create(:term_teacher, id: teacher.id, term: term, teacher: teacher)
+      FactoryBot.create(:term_teacher, term: term, teacher: teacher)
     end
     FactoryBot.create_list(:student, student_count, room: term.room).map do |student|
-      FactoryBot.create(:term_student, id: student.id, term: term, student: student)
+      FactoryBot.create(:term_student, term: term, student: student)
     end
     term.reload
   end
@@ -69,10 +69,10 @@ module FactorySupport
   def create_season_term_with_teacher_and_student(teacher_count, student_count)
     term = create_season_term
     FactoryBot.create_list(:teacher, teacher_count, room: term.room).map do |teacher|
-      FactoryBot.create(:term_teacher, id: teacher.id, term: term, teacher: teacher)
+      FactoryBot.create(:term_teacher, term: term, teacher: teacher)
     end
     FactoryBot.create_list(:student, student_count, room: term.room).map do |student|
-      FactoryBot.create(:term_student, id: student.id, term: term, student: student)
+      FactoryBot.create(:term_student, term: term, student: student)
     end
     term.reload
   end
@@ -80,10 +80,10 @@ module FactorySupport
   def create_exam_planning_term_with_teacher_and_student(teacher_count, student_count)
     term = create_exam_planning_term
     FactoryBot.create_list(:teacher, teacher_count, room: term.room).map do |teacher|
-      FactoryBot.create(:term_teacher, id: teacher.id, term: term, teacher: teacher)
+      FactoryBot.create(:term_teacher, term: term, teacher: teacher)
     end
     FactoryBot.create_list(:student, student_count, room: term.room).map do |student|
-      FactoryBot.create(:term_student, id: student.id, term: term, student: student)
+      FactoryBot.create(:term_student, term: term, student: student)
     end
     term.reload
   end
