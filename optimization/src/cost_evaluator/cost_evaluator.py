718bd16a1ef335bc86fc06b7ffd89072f3f9a7c0
yoshimitsu.masugata <msgtysmt@gmail.com>
2021-05-11 12:57:40 +0900

    cost_evaluatorの追加

--- optimization/src/cost_evaluator/cost_evaluator.py
+++ optimization/src/cost_evaluator/cost_evaluator.py
@@ -8,26 +8,32 @@ class CostEvaluator():
     def __init__(
         self,
         array_size,
-        interval_cutoff,
-        interval_costs,
         student_optimization_rules,
         teacher_optimization_rule,
-        single_cost,
-        different_pair_cost):
+        student_group_occupation,
+        teacher_group_occupation,
+        student_vacancy,
+        teacher_vacancy,
+        school_grades):
         self.__interval_evaluator = IntervalEvaluator(
             array_size=array_size,
-            interval_cutoff=interval_cutoff,
-            interval_costs=interval_costs)
+            student_optimization_rules=student_optimization_rules,
+            school_grades=school_grades)
         self.__occupation_and_blank_evaluator = OccupationAndBlankEvaluator(
             array_size=array_size,
             student_optimization_rules=student_optimization_rules,
-            teacher_optimization_rule=teacher_optimization_rule)
+            teacher_optimization_rule=teacher_optimization_rule,
+            student_group_occupation=student_group_occupation,
+            teacher_group_occupation=teacher_group_occupation,
+            school_grades=school_grades)
         self.__seat_occupation_evaluator = SeatOccupationEvaluator(array_size=array_size)
         self.__seat_combination_evaluator = SeatCombinationEvaluator(
             array_size=array_size,
-            single_cost=single_cost,
-            different_pair_cost=different_pair_cost)
-        self.__vacancy_and_double_booking_evaluator = VacancyAndDoubleBookingEvaluator()
+            single_cost=teacher_optimization_rule.single_cost,
+            different_pair_cost=teacher_optimization_rule.different_pair_cost)
+        self.__vacancy_and_double_booking_evaluator = VacancyAndDoubleBookingEvaluator(
+            student_vacancy=student_vacancy,
+            teacher_vacancy=teacher_vacancy)
 
     def violation_and_cost(self, tutorial_pieces):
         a = self.__interval_evaluator.violation_and_cost(tutorial_pieces)
@@ -35,6 +41,6 @@ class CostEvaluator():
         c = self.__seat_occupation_evaluator.violation_and_cost(tutorial_pieces)
         d = self.__seat_combination_evaluator.violation_and_cost(tutorial_pieces)
         e = self.__vacancy_and_double_booking_evaluator.violation_and_cost(tutorial_pieces)
-        violation = sum(x[0] for x in [a, b, c, d, e])
-        cost = sum(x[1] for x in [a, b, c, d, e])
+        violation = sum(violation_and_cost[0] for violation_and_cost in [a, b, c, d, e])
+        cost = sum(violation_and_cost[1] for violation_and_cost in [a, b, c, d, e])
         return [violation, cost]
