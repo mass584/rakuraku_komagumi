8f2d0d5605fd2b2ab2ee8866ac7f962a490554ea
yoshimitsu.masugata <msgtysmt@gmail.com>
2021-05-20 18:17:15 +0900

    データベースに紐づかないモデルの追加

---
+++ application/app/models/term_overall_schedule.rb
@@ -0,0 +1,97 @@
+class TermOverallSchedule
+  include ActiveModel::Model
+  extend OccupationsBlanks
+
+  attr_accessor :term, :tutorial_pieces, :seats
+
+  validate :verify_daily_blank_limit_for_teacher
+  validate :verify_daily_blank_limit_for_student
+
+  before_validation :fetch_groups_group_by_timetable
+
+  def save
+    term.transaction do
+      term.tutorial_pieces.update_all(seat_id: nil, is_fixed: false)
+      term.seats.update_all(term_teacher_id: nil)
+      seats.each do |seat|
+        record = term.seats.find_by(id: seat[:id])
+        record.update(term_teacher_id: seat[:term_teacher_id])
+      end
+      tutorial_pieces.each do |tutorial_piece|
+        record = term.tutorial_pieces.find_by(id: tutorial_piece[:id])
+        record.update(
+          seat_id: tutorial_piece[:seat_id],
+          is_fixed: tutorial_piece[:is_fixed],
+        )
+      end
+    end
+  end
+
+  private
+
+  # validation
+  def daily_blanks_for_teacher_on_one_day(seats_in_term, term_teacher_id, date_index)
+    tutorials = term.period_index_array.reduce({}) do |accu, period_index|
+      seats_on_this_period = seats_in_term.filter do |seat|
+        seat.date_index == date_index && seat.period_index == period_index
+      end
+      is_exist = seats.find do |seat|
+        seat[:term_teacher_id] == term_teacher_id && seats_on_this_period.include?(seat[:id])
+      end
+      is_exist ? accu.merge({ date_index => is_exist }) : accu
+    end
+    groups = @groups_group_by_timetable[date_index].to_h
+    self.class.daily_blanks_from(term, tutorials, groups)
+  end
+
+  def verify_daily_blank_limit_for_teacher
+    seats_in_term = term.seats.with_index
+    term_teacher_and_date = term.term_teachers.product(term.date_index_array)
+    term_teacher_and_date.reduce(true) do |result, item|
+      blank_limit = item[0].optimization_rule.blank_limit
+      blanks = daily_blanks_for_teacher_on_one_day(seats_in_term, item[0].id, item[1])
+      is_valid = blanks <= blank_limit
+      result && is_valid
+    end
+  end
+
+  def daily_blanks_for_student_on_one_day(seats_in_term, term_student_id, date_index)
+    tutorials = term.period_index_array.reduce({}) do |accu, period_index|
+      seats_on_this_period = seats_in_term.filter do |seat|
+        seat.date_index == date_index && seat.period_index == period_index
+      end
+      is_exist = tutorial_pieces.find do |tutorial_piece|
+        tutorial_piece[:term_student_id] == term_student_id &&
+          seats_on_this_period.include?(tutorial_piece[:seat_id])
+      end
+      is_exist ? accu.merge({ date_index => is_exist }) : accu
+    end
+    groups = @groups_group_by_timetable[date_index].to_h
+    self.class.daily_blanks_from(term, tutorials, groups)
+  end
+
+  def verify_daily_blank_limit_for_student
+    seats_in_term = term.seats.with_index
+    term_student_and_date = term.term_students.product(term.date_index_array)
+    term_student_and_date.reduce(true) do |result, item|
+      blank_limit = item[0].optimization_rule.blank_limit
+      blanks = daily_blanks_for_student_on_one_day(seats_in_term, item[0].id, item[1])
+      is_valid = blanks <= blank_limit
+      result && is_valid
+    end
+  end
+
+  # callback
+  def fetch_groups_group_by_timetable
+    records = term
+              .group_contracts
+              .filter_by_student(tutorial_contract.term_student_id)
+              .filter_by_is_contracted
+              .joins(term_group: :timetables)
+              .select(:term_student_id, :date_index, :period_index)
+    @groups_group_by_timetable = records.group_by_recursive(
+      proc { |item| item[:date_index] },
+      proc { |item| item[:period_index] },
+    )
+  end
+end
---
+++ application/app/models/term_schedule.rb
@@ -0,0 +1,211 @@
+class TermSchedule
+  include ActiveModel::Model
+  extend OccupationsBlanks
+
+  attr_accessor :tutorial_piece_id, :seat_id
+
+  validate :verify_teacher_daily_blank_limit
+  validate :verify_student_daily_blank_limit
+
+  before_validation :fetch_tutorials_group_by_teacher_and_timetable
+  before_validation :fetch_groups_group_by_teacher_and_timetable
+  before_validation :fetch_tutorials_group_by_student_and_timetable
+  before_validation :fetch_groups_group_by_student_and_timetable
+
+  def save
+    if creation?
+      creation_transaction
+    elsif updation?
+      updation_transaction
+    elsif deletion?
+      deletion_transaction
+    end
+  end
+
+  private
+
+  def tutorial_piece
+    @tutorial_piece ||= TutorialPiece.find_by(id: tutorial_piece_id)
+    @tutorial_piece
+  end
+
+  def before_seat
+    @before_seat ||= @tutorial_piece.seat
+    @before_seat
+  end
+
+  def after_seat
+    @after_seat ||= Seat.find_by(id: seat_id)
+    @after_seat
+  end
+
+  def term_teacher
+    @term_teacher ||= TermTeacher.find_by(
+      id: tutorial_piece.tutorial_contract.term_teacher_id,
+    )
+    @term_teacher
+  end
+
+  def term_student
+    @term_student ||= TermStudent.find_by(
+      id: tutorial_piece.tutorial_contract.term_student_id,
+    )
+    @term_student
+  end
+
+  def empty_before_seat?
+    before_seat.tutorial_pieces.count == 1
+  end
+
+  def creation?
+    before_seat.nil? && after_seat.present?
+  end
+
+  def updation?
+    before_seat.present? && after_seat.present? && before_seat.id != after_seat.id
+  end
+
+  def deletion?
+    before_seat.present? && after_seat.nil?
+  end
+
+  def creation_transaction
+    ActiveRecord::Base.transaction do
+      after_seat.update(term_teacher_id: term_teacher.id)
+      tutorial_piece.update(seat_id: after_seat.id)
+    end
+  end
+
+  def updation_transaction
+    ActiveRecord::Base.transaction do
+      if empty_before_seat?
+        before_seat.update(term_teacher_id: nil)
+      end
+      after_seat.update(term_teacher_id: term_teacher.id)
+      tutorial_piece.update(seat_id: after_seat.id)
+    end
+  end
+
+  def deletion_transaction
+    ActiveRecord::Base.transaction do
+      if empty_before_seat?
+        before_seat.update(term_teacher_id: nil)
+      end
+      after_seat.update(term_teacher_id: term_teacher.id)
+      tutorial_piece.update(seat_id: nil)
+    end
+  end
+
+  # validation
+  def daily_blanks_for_teacher_on_before_seat
+    date_index = before_seat.timetable.date_index
+    period_index = before_seat.timetable.period_index
+    tutorials = @tutorials_group_by_teacher_and_timetable
+                .dig(term_teacher.id, date_index).to_h
+    tutorials.delete(period_index) if empty_before_seat?
+    groups = @groups_group_by_teacher_and_timetable
+             .dig(term_teacher.id, date_index).to_h
+    self.class.daily_blanks_from(term, tutorials, groups)
+  end
+
+  def daily_blanks_for_teacher_on_after_seat
+    date_index = after_seat.timetable.date_index
+    period_index = after_seat.timetable.period_index
+    tutorials = @tutorials_group_by_teacher_and_timetable
+                .dig(term_teacher.id, date_index).to_h
+    tutorials.merge!({ period_index => true })
+    groups = @groups_group_by_teacher_and_timetable
+             .dig(term_teacher.id, date_index).to_h
+    self.class.daily_blanks_from(term, tutorials, groups)
+  end
+
+  def daily_blanks_for_student_on_before_seat
+    date_index = before_seat.timetable.date_index
+    period_index = before_seat.timetable.period_index
+    tutorials = @tutorials_group_by_student_and_timetable
+                .dig(term_student.id, date_index).to_h
+    tutorials.delete(period_index)
+    groups = @groups_group_by_student_and_timetable
+             .dig(term_student.id, date_index).to_h
+    self.class.daily_blanks_from(term, tutorials, groups)
+  end
+
+  def daily_blanks_for_student_on_after_seat
+    date_index = after_seat.timetable.date_index
+    period_index = after_seat.timetable.period_index
+    tutorials = @tutorials_group_by_student_and_timetable
+                .dig(term_student.id, date_index).to_h
+    tutorials.merge!({ period_index => true })
+    groups = @groups_group_by_student_and_timetable
+             .dig(term_student.id, date_index).to_h
+    self.class.daily_blanks_from(term, tutorials, groups)
+  end
+
+  def verify_teacher_daily_blank_limit
+    if (creation? || updation?) &&
+       daily_blanks_for_teacher_on_after_seat > term_teacher.optimization_rule.blank_limit
+      errors.add(:base, '講師の１日の空きコマの上限を超えています')
+    end
+
+    if (updation? || deletion?) &&
+       daily_blanks_for_teacher_on_before_seat > term_teacher.optimization_rule.blank_limit
+      errors.add(:base, '講師の１日の空きコマの上限を超えています')
+    end
+  end
+
+  def verify_student_daily_blank_limit
+    if (creation? || updation?) &&
+       daily_blanks_for_student_on_after_seat > term_teacher.optimization_rule.blank_limit
+      errors.add(:base, '生徒の１日の空きコマの上限を超えています')
+    end
+
+    if (updation? || deletion?) &&
+       daily_blanks_for_student_on_before_seat > term_teacher.optimization_rule.blank_limit
+      errors.add(:base, '生徒の１日の空きコマの上限を超えています')
+    end
+  end
+
+  # callback
+  def fetch_tutorials_group_by_teacher_and_timetable
+    records = term.seats.joins(:timetable)
+                  .select(:id, :term_teacher_id, :date_index, :period_index)
+                  .select { |item| item[:term_teacher_id].present? }
+    @tutorials_group_by_teacher_and_timetable = records.group_by_recursive(
+      proc { |item| item[:term_teacher_id] },
+      proc { |item| item[:date_index] },
+      proc { |item| item[:period_index] },
+    )
+  end
+
+  def fetch_groups_group_by_teacher_and_timetable
+    records = term.timetables.joins(term_group: :term_group_term_teachers)
+                  .select(:term_teacher_id, :date_index, :period_index)
+                  .select { |item| item[:term_teacher_id].present? }
+    @groups_group_by_teacher_and_timetable = records.group_by_recursive(
+      proc { |item| item[:term_teacher_id] },
+      proc { |item| item[:date_index] },
+      proc { |item| item[:period_index] },
+    )
+  end
+
+  def fetch_tutorials_group_by_student_and_timetable
+    records = term.tutorial_pieces.joins(:tutorial_contract, seat: :timetable)
+                  .select(:term_student_id, :date_index, :period_index)
+    @tutorials_group_by_student_and_timetable = records.group_by_recursive(
+      proc { |item| item[:term_student_id] },
+      proc { |item| item[:date_index] },
+      proc { |item| item[:period_index] },
+    )
+  end
+
+  def fetch_groups_group_by_student_and_timetable
+    records = term.group_contracts.filter_by_is_contracted
+                  .joins(term_group: :timetables)
+                  .select(:term_student_id, :date_index, :period_index)
+    @groups_group_by_student_and_timetable = records.group_by_recursive(
+      proc { |item| item[:term_student_id] },
+      proc { |item| item[:date_index] },
+      proc { |item| item[:period_index] },
+    )
+  end
+end
