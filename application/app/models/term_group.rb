5f99974eab31965cea308b28d3d11b2b68a44971
yoshimitsu.masugata <msgtysmt@gmail.com>
2021-05-25 19:20:00 +0900

    集団担任の変更バリデーションを削除

--- application/app/models/term_group.rb
+++ application/app/models/term_group.rb
@@ -6,6 +6,8 @@ class TermGroup < ApplicationRecord
   has_many :group_contracts, dependent: :restrict_with_exception
   has_many :timetables, dependent: :nullify
 
+  validate :tutorial_piece_empty?
+
   before_create :set_nest_objects
 
   scope :ordered, lambda {
@@ -15,9 +17,18 @@ class TermGroup < ApplicationRecord
     joins(:group).select('term_groups.*', 'groups.name')
   }
 
+  def editable
+    !term.tutorial_pieces.filter_by_placed.exists?
+  end
+
   private
 
-  # before_create
+  def tutorial_piece_empty?
+    unless editable
+      errors.add(:base, 'コマが配置されているため変更できません')
+    end
+  end
+
   def set_nest_objects
     self.group_contracts = new_group_contracts
   end
