a598360bc7383f0faaf28e06b3098b861c5cffa8
yoshimitsu.masugata <msgtysmt@gmail.com>
2021-06-04 14:44:35 +0900

    optimizationのサブモジュール化

--- optimization/test/__init__.py
+++
@@ -1,6 +0,0 @@
-import logging
-import sys
-
-format = "%(asctime)s %(levelname)s %(name)s :%(message)s"
-logging.basicConfig(level='INFO', filename='log/test.log', format=format)
-sys.path.append('./src')
